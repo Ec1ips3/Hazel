eb4f66d9e75c2eba84cf41d2a9e16c25d0a447c9
Ec1ips3 <gsv.eclipse@gmail.com>
2020-06-12 21:37:21 +1000

    Created Entry Point. and base files

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,25 @@
+#include "Application.h"
+
+//#include "Events/ApplicationEvent.h"
+//#include"Log.h"
+
+namespace Hazel {
+
+	Application::Application()
+	{
+	}
+
+
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		//WindowResizeEvent e(1280, 720);
+		//HZ_TRACE(e);
+
+		while (true);
+	}
+
+}
