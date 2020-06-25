d5d03814da0b5f4032361148b8f28c141778206f
Ec1ips3 <gsv.eclipse@gmail.com>
2020-06-25 22:21:42 +1000

    To First Pink Window

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -10,4 +10,12 @@
 	#error Hazel only supports Windows!
 #endif // HZ_PLATFORM_WINDOWS
 
-//#define BIT(x) (1 << x)
+#ifdef HZ_ENABLE_ASSERTS
+	#define HZ_ASSERT(x, ...) {if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define HZ_CORE_ASSERT(x, ...) {if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define HZ_ASSERTS(x, ...)
+	#define HZ_CORE_ASSERT(x, ...)
+#endif // HZ_ENABLE_ASSERTS
+
+#define BIT(x) (1 << x)
---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "Hazel/Events/Event.h"
+//#include "Hazel/Core/Input.h"
+
+#include "Event.h"
+
+namespace Hazel {
+
+	class HAZEL_API KeyEvent : public Event
+	{
+	public:
+		//KeyCode GetKeyCode() const { return m_KeyCode; }
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		//KeyEvent(KeyCode keycode)
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		//KeyCode m_KeyCode;
+		int m_KeyCode;
+	};
+
+	class HAZEL_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		//KeyPressedEvent(KeyCode keycode, int repeatCount)
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	private:
+		int m_RepeatCount;
+	};
+
+	class HAZEL_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		//KeyReleasedEvent(KeyCode keycode)
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+
+	/*class HAZEL_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(KeyCode keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};*/
+}
