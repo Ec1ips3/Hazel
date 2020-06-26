addfa1131cd4e7e10d035f3e717b059ba4841383
Ec1ips3 <gsv.eclipse@gmail.com>
2020-06-26 16:42:21 +1000

    Fixed Layers section... (incorrect Platform folder placement)

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,32 @@
 #include <Hazel.h>
 
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		HZ_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Hazel::Event& event) override
+	{
+		HZ_TRACE("{0}", event);
+	}
+
+};
+
+
 class Sandbox : public Hazel::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 	~Sandbox()
 	{
@@ -16,46 +37,4 @@ public:
 Hazel::Application* Hazel::CreateApplication()
 {
 	return new Sandbox();
-
 }
-
-//#include <Hazel.h>
-//
-//class ExampleLayer : public Hazel::Layer
-//{
-//public:
-//	ExampleLayer()
-//		: Layer("Example")
-//	{
-//	}
-//
-//	void OnUpdate() override
-//	{
-//		HZ_INFO("ExampleLayer::Update");
-//	}
-//
-//	void OnEvent(Hazel::Event& event) override
-//	{
-//		HZ_TRACE("{0}", event);
-//	}
-//
-//};
-//
-//
-//class Sandbox : public Hazel::Application
-//{
-//public:
-//	Sandbox()
-//	{
-//		PushLayer(new ExampleLayer());
-//	}
-//	~Sandbox()
-//	{
-//
-//	}
-//};
-//
-//Hazel::Application* Hazel::CreateApplication()
-//{
-//	return new Sandbox();
-//}
